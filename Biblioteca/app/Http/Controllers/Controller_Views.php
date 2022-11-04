0c77469667bd8d977fd85509f2b0dc14339ccf48
AlanRLA <lopezz.alan134@gmail.com>
2022-11-04 14:39:33 -0600

    Mensajes v1
    Mensajes de validación e intento del sweatalert

--- Biblioteca/app/Http/Controllers/Controller_Views.php
+++ Biblioteca/app/Http/Controllers/Controller_Views.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\validation_form;
+use Illuminate\Support\Facades\Redirect;
 
 class Controller_Views extends Controller
 {
@@ -15,8 +16,13 @@ class Controller_Views extends Controller
         return view('Registro de libro');
     }
 
-    public function ProcesarLibro(validation_form $req){
-        return 'Se esta procesanding';
+    public function ProcesarLibro(validation_form $req){     
+
+        $titulo = $req->txtTitulo;
+
+        
+
+        return Redirect('Registro')->with('Confirmación',[$titulo]);
     }
 
 }
--- Biblioteca/resources/views/Registro de libro.blade.php
+++ Biblioteca/resources/views/Registro de libro.blade.php
@@ -1,10 +1,21 @@
 @extends('PLantilla')
 
 @section('contenido')
-    
+
+    @if (session()->has('Confirmación','titulo'))
+       {!!"<script> 
+       Swal.fire(
+        'Todo correcto!',
+        'Libro guardado',
+        'success'
+         )
+         </script>"!!}
+    @endif
+
     <div class="container mt-4 col-md-4">
         <h4 class="text-center">Registro de libro</h4>
         
+        {{-- IF de mensajes
         @if ($errors->any())
             @foreach ($errors->all() as $error)
                 <div class="alert alert-danger" role="alert">
@@ -13,42 +24,50 @@
                 </div>  
             @endforeach                   
         @endif
-
+        --}}       
         <div class="card text-dark bg-light mb-3">
             <div class="card-header">Formulario de registro</div>
              <div class="card-body">
+
                 <form action="GuardarLibro" method="POST">
                  @csrf
                     <div>
                         <label>ISBN</label>
-                        <input class="form-control" type="text" name="nmISBN" placeholder="Ingresa ISBN">
+                        <input class="form-control" type="text" name="nmISBN" placeholder="Ingresa ISBN" value="{{old('nmISBN')}}">
+                        <p class="text-danger">{{$errors->first('nmISBN')}}</p>         
                     </div>
                     <div class="mt-2">
                         <label>Título</label>
-                        <input class="form-control" type="text" name="txtTitulo" placeholder="Ingresa título">
+                        <input class="form-control" type="text" name="txtTitulo" placeholder="Ingresa título" value="{{old('txtTitulo')}}">
+                        <p class="text-danger">{{$errors->first('txtTitulo')}}</p>
                     </div>
                     <div class="mt-2">
                         <label>Autor</label>
-                        <input class="form-control" type="text" name="txtAutor" placeholder="Ingresa autor">
+                        <input class="form-control" type="text" name="txtAutor" placeholder="Ingresa autor" value="{{old('txtAutor')}}">
+                        <p class="text-danger">{{$errors->first('txtAutor')}}</p>
                     </div>  
                     <div class="mt-2">
                         <label>Páginas</label>
-                        <input class="form-control" type="text" name="nmPaginas" placeholder="Ingresa cantidad de páginas">
+                        <input class="form-control" type="text" name="nmPaginas" placeholder="Ingresa cantidad de páginas" value="{{old('nmPaginas')}}">
+                        <p class="text-danger">{{$errors->first('nmPaginas')}}</p>
                     </div>     
                     <div class="mt-2">
                         <label>Editorial</label>
-                        <input class="form-control" type="text" name="txtEditorial" placeholder="Ingresa editorial">
+                        <input class="form-control" type="text" name="txtEditorial" placeholder="Ingresa editorial" value="{{old('txtEditorial')}}">
+                        <p class="text-danger">{{$errors->first('txtEditorial')}}</p>
                     </div> 
                     <div class="mt-2 mb-2">
                         <label>Email Editorial</label>
-                        <input class="form-control" type="text" name="emEditorial" placeholder="Ingresa email de la editorial">
+                        <input class="form-control" type="text" name="emEditorial" placeholder="Ingresa email de la editorial" value="{{old('emEditorial')}}">
+                        <p class="text-danger">{{$errors->first('emEditorial')}}</p>
                     </div> 
                     
                     <div class="text-center">
                        <button type="submit" class="btn btn-success" name="btn_s">Guardar</button> 
                     </div>
                     
-                </form>               
+                </form>      
+
             </div>
         </div>
     </div>
