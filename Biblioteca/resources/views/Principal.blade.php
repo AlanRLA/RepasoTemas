ad13c4b8133f76dbdad68b1c68cb13d8f137dc08
AlanRLA <lopezz.alan134@gmail.com>
2022-11-04 11:20:17 -0600

    Vistas
    Se incluyeron las cosas que pide la practica en cada vista

--- Biblioteca/resources/views/Principal.blade.php
+++ Biblioteca/resources/views/Principal.blade.php
@@ -2,8 +2,39 @@
 
 @section('contenido')
     
-    <div class="container mt-4 col-md-4">
+    <div class="container mt-4 col-md-10">
         <h4 class="text-center">Bienvenido a la biblioteca</h4>
 
+        <div class="card">
+            <div class="card-header">
+              <STRong>Entrevista</STRong> Allan Poe
+            </div>
+            <div class="card-body">
+              <h3 class="card-title text-center" style="font-family: 'Times New Roman', Times, serif; font-size:50px" ><strong>"En el amor todos somos personajes, pero él dejo la tinta y ella la actuación"</strong></h3>
+              <br>      
+              <p class="card-text" style="font-family: 'Times New Roman', Times, serif; font-size:20px;">
+                Entrevista al Poeta, narrador y crítico estadounidense Edgar Allan Poe. El día 22 de enero del 1846, nos fue concedida  una entrevista por el señorEdgar Allan Poe, autor del cuento nombrado “El corazón delator” El motivo de dicha entrevista es pregúntale aspectos desde su punto de vistasobre   dicho  cuento,     cuestiones  o  interrogantes   suscitadas  por   sus  mismolectores. 
+              </p>
+              
+              <div class="container px-4">
+                <div class="row gx-5">
+                  <div class="col">
+                   <div class="p-3 border bg-light" style="font-family: 'Times New Roman', Times, serif; font-size:18px">
+                    -Sofia: Buenas tardes Sr. Allan , hemos venido para entrevistarlo y que nos responda algunas preguntas realizadas por lectores de su cuento “El corazóndelator”,  ya que ha habido aspectos que han quedado en duda, y nos gustaría que nos aclare. <br> -Edgar: Hola Sofía, gracias por invitarme, dime ¿Cuáles son las preguntas? <br>-Sofia: Bueno en primer lugar nos preguntan qué ¿en que usted se inspirópara escribir el cuánto?  <br>-Edgar: Mira, con este cuento me quiero enfocar que la consciencia puede llegar a veces a ser un gran enemigo en el ente humano cuando su objetivo escubrir el pecado, el crimen.
+                   </div>
+                </div>
+                  <div class="col">
+                    <div class="p-3 border bg-light">
+                      <img src="https://i0.wp.com/elitediario.com/wp-content/uploads/2020/02/ED-002-Alan-Poe.jpg?fit=800%2C451&ssl=1" width="400" height="250">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer text-muted">
+              Hace un mes por Sofia Murillo
+            </div>
+        </div>
     </div>
+    <br>
 @endsection
--- Biblioteca/resources/views/Registro de libro.blade.php
+++ Biblioteca/resources/views/Registro de libro.blade.php
@@ -4,6 +4,42 @@
     
     <div class="container mt-4 col-md-4">
         <h4 class="text-center">Registro de libro</h4>
-
+        
+        <div class="card text-dark bg-light mb-3">
+            <div class="card-header">Formulario de registro</div>
+             <div class="card-body">
+                <form action="">
+                    <div>
+                        <label>ISBN</label>
+                        <input class="form-control" type="text" placeholder="Ingresa ISBN">
+                    </div>
+                    <div class="mt-2">
+                        <label>Título</label>
+                        <input class="form-control" type="text" placeholder="Ingresa título">
+                    </div>
+                    <div class="mt-2">
+                        <label>Autor</label>
+                        <input class="form-control" type="text" placeholder="Ingresa autor">
+                    </div>  
+                    <div class="mt-2">
+                        <label>Páginas</label>
+                        <input class="form-control" type="number" placeholder="Ingresa cantidad de páginas">
+                    </div>     
+                    <div class="mt-2">
+                        <label>Editorial</label>
+                        <input class="form-control" type="text" placeholder="Ingresa editorial">
+                    </div> 
+                    <div class="mt-2 mb-2">
+                        <label>Email Editorial</label>
+                        <input class="form-control" type="email" placeholder="Ingresa email de la editorial">
+                    </div> 
+                    
+                    <div class="text-center">
+                       <button type="button" class="btn btn-success">Guardar</button> 
+                    </div>
+                    
+                </form>               
+            </div>
+        </div>
     </div>
 @endsection
